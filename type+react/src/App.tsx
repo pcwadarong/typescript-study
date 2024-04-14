491c015931f2cbe77277df0e3f2c57adb1dec66e
Chaewon <cw06155@gmail.com>
2024-04-15 06:45:23 +0900

    basic preset

--- type+react/src/App.tsx
+++ type+react/src/App.tsx
@@ -1,11 +1,10 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div className="App">
+    </div>
+  );
 }
 
-export default App
+export default App;

377e2893b4d3992e3709ef011f6d18c164bb6da7
Chaewon <cw06155@gmail.com>
2024-04-15 07:55:16 +0900

    add read me

--- src/test.ts
+++ src/test.ts
@@ -1,16 +1,2 @@
 console.log("hello");
 let a: number = 5;
-
-interface Post {
-  title: string;
-  tags: string[];
-  content: string;
-  thumbnailURL?: string;
-}
-
-const draft: Required<Post> = { // ❌
-    title: "한입 타스 후기",
-    tags: ["ts"],
-    content: "",
-    // thumbnailURL: "https://...",
-  };

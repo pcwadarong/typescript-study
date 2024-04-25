37e1bc0eca3a26ba144913dd4b42575990eed978
Chaewon <cw06155@gmail.com>
2024-04-25 10:33:02 +0900

    change useState to useReducer

--- type+react - todolist/src/App.tsx
+++ type+react - todolist/src/App.tsx
@@ -1,28 +1,59 @@
 import './App.css';
-import { useState, useRef } from 'react';
+import { useRef, useReducer, useEffect } from 'react';
 import Editor from './components/Editor';
 import TodoItem from './components/TodoItem';
 import { Todo } from './types';
 
+// 타입 스크립트에서는 액션 객체의 타입을 주로 서로소 유니온으로 선언
+type Action =
+  | {
+      type: 'CREATE';
+      data: {
+        id: number;
+        content: string;
+      };
+    }
+  | { type: 'DELETE'; id: number };
+
+function reducer(state: Todo[], action: Action) {
+  switch (action.type) {
+    case 'CREATE': {
+      return [...state, action.data];
+    }
+    case 'DELETE': {
+      return state.filter((item) => item.id !== action.id);
+    }
+  }
+}
+
 function App() {
   // Todo 타입을 배열로 가져오는 타입 정의
-  const [todos, setTodos] = useState<Todo[]>([]);
+  //const [todos, setTodos] = useState<Todo[]>([]);
+  // useReducer로 대치
+  const [todos, dispatch] = useReducer(reducer, []);
 
   // id를 하나씩 증가하도록 설정하기 위한 ref
   const idRef = useRef(0);
 
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
   const onClickAdd = (text: string) => {
-    setTodos([
-      ...todos,
-      {
+    dispatch({
+      type: 'CREATE',
+      data: {
         id: idRef.current++,
         content: text,
       },
-    ]);
+    });
   };
 
   const onClickDelete = (id: number) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
+    dispatch({
+      type: 'DELETE',
+      id: id,
+    });
   };
 
   return (
--- type+react - todolist/src/components/TodoItem.tsx
+++ type+react - todolist/src/components/TodoItem.tsx
@@ -1,4 +1,5 @@
 import { Todo } from '../types';
+
 interface Props extends Todo {
   onClickDelete: (id: number) => void;
 }
